aa2e1dcb11ea47a60a37cd2f95704d86438d34aa
vlada061098@gmail.com <vlada061098@gmail.com>
2020-09-08 19:46:42 +0300

    Feat: Add storage for messages //should be commited in 6b8ee6403d60071c78916c3fe5a944f6071a692c

---
+++ PadLab1Broker/PadLab1Broker/PayloadStorage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadLab1Broker
+{
+    static class PayloadStorage
+    {
+        private static ConcurrentQueue<Payload> payloadQueue;
+
+        static PayloadStorage()
+        {
+            payloadQueue = new ConcurrentQueue<Payload>();
+        }
+
+        public static void Add(Payload payload)
+        {
+            payloadQueue.Enqueue(payload);
+        }
+
+        public static Payload GetNext()
+        {
+            Payload payload = null;
+            payloadQueue.TryDequeue(out payload);
+            return payload;
+        }
+
+        public static bool isEmpty()
+        {
+            return payloadQueue.IsEmpty;
+        }
+    }
+}
