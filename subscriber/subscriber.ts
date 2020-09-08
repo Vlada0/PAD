966c418567dad8324713ab7fd3137cf4a43681ed
Angelina <angiestavnk@gmail.com>
2020-09-08 18:34:54 +0300

    feat: add subscriber

---
+++ subscriber/subscriber.ts
@@ -0,0 +1,55 @@
+import * as net from "net";
+import { Socket } from "dgram";
+const readline = require('readline');
+const client = new net.Socket();
+const port = 11000;
+const host = '127.0.0.1';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+client.connect(port, host, function() {
+    rl.question('Enter topic:', (topic) => {
+        subscribeOn(topic);
+    })
+})
+
+rl.on('line', (topic) => {
+    console.log(topic);
+    let arr = topic.split(' ');
+    if(arr.shift() === 'rem') {
+        unsubscribeOn(arr.join(' '));
+    } else {
+        subscribeOn(topic);
+    }
+})
+
+client.on('data', function(data) {
+    let json = JSON.parse(data.toString());
+    let statusCode = json.statusCode;
+
+    if(typeof statusCode !== 'undefined' && statusCode !== 200) {
+        console.log('Cannot send your message');
+    } else {
+        console.log(json.username + ' ' + json.topic + ' ' + json.message);
+    }
+
+})
+
+function subscribeOn(topic) {
+    let data = {
+        subscribe: topic
+    }
+    let jsonData = JSON.stringify(data);
+    client.write(jsonData);
+}
+
+function unsubscribeOn(topic) {
+    let data = {
+        unsubscribe: topic
+    }
+    let jsonData = JSON.stringify(data);
+    client.write(jsonData);
+}
